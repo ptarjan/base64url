741ab84aac15607b4fd78e6d7cc3c210f279764c
Paul Tarjan <paul@paulisageek.com>
2010-07-22 12:41:12 -0700

    remove comment

--- python.py
+++ python.py
@@ -1,6 +1,5 @@
 import base64
 def base64_url_decode(input):
-  #pythons base64 is close, but requires all the padding
   input += '=' * (4 - (len(input) % 4))
   return base64.urlsafe_b64decode(input)
 
