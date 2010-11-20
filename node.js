91dc3b9980f8d4000cdb6c6f2cfd2b6193246e9e
François de Metz <francois@2metz.fr>
2010-11-20 08:15:51 +0800

    fixe some jslint error

--- node.js
+++ node.js
@@ -1,17 +1,16 @@
-var base64 = require('base64') // provided by https://github.com/pkrumins/node-base64
-var assert = require('assert')
-var fs     = require('fs')
+var base64 = require('base64'); // provided by https://github.com/pkrumins/node-base64
+var assert = require('assert');
+var fs     = require('fs');
 
 function base64_url_decode(input) {
-    return base64.decode(input.replace(/-/g, '+').replace(/_/g, '/'))
+    return base64.decode(input.replace(/-/g, '+').replace(/_/g, '/'));
 }
 
-var tests = fs.readFileSync(__dirname+"/tests.txt").toString().split("\n")
+var tests = fs.readFileSync(__dirname+"/tests.txt").toString().split("\n");
 
 tests.forEach(function(test) {
-    if (!test || test[0] == '#') return
-    //  list($name, $input, $output) = explode(' ', $test);
-    var line = test.split(" ")
+    if (!test || test[0] == '#') { return; }
+    var line = test.split(" ");
     var data = base64_url_decode(line[1]);
     assert.equal(data, line[2], line[0]);
-})
+});
