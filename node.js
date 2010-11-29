c005a2ce91e07e68ef5bcdcb94c93ab563bbc788
Paul Tarjan <paul@paulisageek.com>
2010-11-29 00:46:50 -0800

    format node.js to be similar to the other examples

--- node.js
+++ node.js
@@ -1,16 +1,21 @@
 var base64 = require('base64'); // provided by https://github.com/pkrumins/node-base64
-var assert = require('assert');
 var fs     = require('fs');
+var sys    = require('sys');
 
 function base64_url_decode(input) {
     return base64.decode(input.replace(/-/g, '+').replace(/_/g, '/'));
 }
 
-var tests = fs.readFileSync(__dirname+"/tests.txt").toString().split("\n");
+var tests = fs.readFileSync(__dirname+'/tests.txt').toString().split("\n");
 
 tests.forEach(function(test) {
     if (!test || test[0] == '#') { return; }
     var line = test.split(" ");
-    var data = base64_url_decode(line[1]);
-    assert.equal(data, line[2], line[0]);
+    name = line[0];
+    input = line[1];
+    output = line[2];
+    var data = base64_url_decode(input);
+    if (data !== output) {
+      sys.puts('node.js: '+name+' failed. '+data+' != '+output+"\n");
+    }
 });
